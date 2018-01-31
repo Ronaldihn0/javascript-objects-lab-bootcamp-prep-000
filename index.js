fc38817c5502398ea83af2366bc19cb34e36e8f1
Ronald DeBeras <rdeberas@gmail.com>
2018-01-31 23:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ function deleteFromObjectByKey (object, key, value) {
 object[key] = value;
 return object
 }
+
+function deleteFromObjectByKey (object, key, value){
+object[key] = value;
+
+return object
+}
+
+var object = { key: 'value' }
+ 
+var newObject = Object.assign({}, object)
