fba17e880bc9335a6af503cef0c75652f4771272
Ronald DeBeras <rdeberas@gmail.com>
2018-01-31 23:53:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue (object, key, value) {
+Object.key = value;
+return Object
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+Object.key = value;
+return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+object[key] = value;
+return object
+}
+
+function deleteFromObjectByKey (object, key, value) {
+object[key] = value;
+return object
+}
