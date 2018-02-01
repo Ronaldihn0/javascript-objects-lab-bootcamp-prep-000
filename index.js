4e023c97c957370a190aa6e8d7bf9b422e3a06cc
Ronald DeBeras <rdeberas@gmail.com>
2018-02-01 00:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ object[key] = value;
 return object
 }
 
-function deleteFromObjectByKey (object, key) {
+function deleteFromObjectByKey (object, key, value) {
   delete object[key];
   return object
+}
+function deleteFromObjectByKey(object, key) {
+var clone = Object.assign({}, object);
+delete clone[key];
+return clone;
 }
