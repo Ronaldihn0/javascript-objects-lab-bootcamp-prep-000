ce47ff44aa83d3a3b43fb78a4c5273154b9a2a49
Ronald DeBeras <rdeberas@gmail.com>
2018-02-01 00:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ object[key] = value;
 return object
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-delete object[key];
-return object;
+function deleteFromObjectByKey(object, key) {
+var clone = Object.assign({}, object);
+delete clone[key];
+return clone;
